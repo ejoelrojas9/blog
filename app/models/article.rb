208a40257cc8e91834ea455defe5cb1a991e0af4
Joel Rojas <ejoelrojas9@gmail.com>
2022-01-30 00:15:20 -0500

    Mi primer proyecto de Ruby on Rails con las clases de codigofacilito

---
+++ app/models/article.rb
@@ -0,0 +1,5 @@
+class Article < ApplicationRecord
+    # La tabla => articles (Article pasado a plural)
+    # Campos => article.title() => Titulo del articulo y todos los demas campos
+    # Escribir metodos
+end
