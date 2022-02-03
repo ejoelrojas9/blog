e074adda7a82ba3e27cb31b968da296b6d8fa62d
Joel Rojas <ejoelrojas9@gmail.com>
2022-02-03 18:10:20 -0500

    Videos 10 y 11 de codigofacilito

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,17 @@
  class ArticlesController < ApplicationController
 
-    #Get /articles (definicion de index)
+    #Get /articles (definicion de index) Select * FROM articles
     def index
+        #En cuentra todos los registros
         @articles = Article.all 
     end
 
     # Get /articles/:id (show, lectura)
     def show
+        #Encuentra un registro por su id
         @article = Article.find(params[:id])
+        #where
+        #Article.where.not("id = ?",params[:id])
     end 
 
     # Get /articles/new
@@ -17,14 +21,26 @@
 
     #POST /article (Save article)
     def create
-        @article = Article.new(title: params[:article][:title], body: params[:article][:title])
-        @article.save
-        redirect_to @article
+        @article = Article.new(title: params[:article][:title], body: params[:article][:body])
+        if @article.save
+            redirect_to @article
+        else
+            render :new
+        end
+    end
+
+    #DELETE /articles/:id
+    def destroy
+        #DELETE FROM articles
+        @article = Article.find(params[:id])
+        @article.destroy #Destroy elimina el objeto
+        redirect_to articles_path
     end
 
     #PUT /articles/:id (update)
     def update
-        
+       # UPDATe
+         #@article.update_atributes({title: 'Nuevo titulo'})   
     end
 
- end    
+ end
--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,6 @@ class Article < ApplicationRecord
     # La tabla => articles (Article pasado a plural)
     # Campos => article.title() => Titulo del articulo y todos los demas campos
     # Escribir metodos
-    validades :title, presence: true
-    validades :body, presence: true
+    validates :title, presence: true, uniqueness: true
+    validates :body, presence: true, length: { minimum: 20 }
 end
