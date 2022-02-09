884fddb871b3db7a4e63dbd5fdc8b80232138bb9
Joel Rojas <ejoelrojas9@gmail.com>
2022-02-09 08:15:22 -0500

    video  12 y 13 codigo facilito

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,9 +19,15 @@
         @article = Article.new
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
     #POST /article (Save article)
     def create
-        @article = Article.new(title: params[:article][:title], body: params[:article][:body])
+        # @article = Article.new(title: params[:article][:title], body: params[:article][:body]) es igual a la siguiente linea
+        # @article = Article.new(params[:article]) {title: "Titulo", body: "Cuerpo !"}
+        @article = Article.new(article_params)
         if @article.save
             redirect_to @article
         else
@@ -41,6 +47,17 @@
     def update
        # UPDATe
          #@article.update_atributes({title: 'Nuevo titulo'})   
+         @article = Article.find(params[:id])
+         if @article.update(article_params)
+            redirect_to @article
+         else
+            render :edit
+         end
+    end
+
+    private
+    def article_params
+        params.require(:article).permit(:title,:body)
     end
 
  end
